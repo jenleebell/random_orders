e92daa490908940c8c3bff449f8460e85493ad44
Jennifer <jenleebell@gmail.com>
2015-11-15 13:38:02 -0800

    Update the code

--- lib/order.rb
+++ lib/order.rb
@@ -1,16 +1,18 @@
-class Random_Order
+class Order
   define_method(:initialize) do |quantity, method|
     @quantity = quantity
     @method = method
     @total_orders = []
   end
 
-  until @total_orders.length == 100
-    @quantity = rand(1..100)
-    @method = rand(1..6)
-    order = Random_Order.new(quantity, method)
-    total_orders.push(order)
-  end
+  define_method(:randomized_orders)
 
-  return total_orders
+    until @total_orders.length == 100
+      @quantity = rand(1..100)
+      @method = rand(1..6)
+      order = Order.new(quantity, method)
+      total_orders.push(order)
+    end
+
+    return total_orders
 end
